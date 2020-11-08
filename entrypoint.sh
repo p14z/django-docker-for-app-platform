9439a2013d5b6b0c00a76d39c3b9829130b27b46
Pedro <48163273+p14z@users.noreply.github.com>
2020-11-08 10:46:38 -0500

    Increase threads

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py compilemessages
 
-gunicorn --bind 0.0.0.0:8080 --workers 3 mysite.wsgi:application
+gunicorn --bind 0.0.0.0:8080 --workers=3 --threads=2 mysite.wsgi:application
